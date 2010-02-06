b7e128e2c1e165e6cea8e73e772a896469d8afb7
Mark Ryall <mark@ryall.name>
2010-02-06 23:27:05 +1100

    added a browser

--- lib/splat.rb
+++ lib/splat.rb
@@ -2,20 +2,26 @@ class Splat
   attr_reader :platform
 
   def initialize
-    @platform = :unknown
-    if Config::CONFIG['host_os'] =~ /mswin|win32|dos|cygwin|mingw/i
-      @platform = :win32
-      require 'splat/win32_clipboard'
-      @clipboard = Splat::Win32Clipboard.new
-      require 'splat/win32_launcher'
-      @launcher = Splat::Win32Launcher.new
-    end
-    if Config::CONFIG['host_os'] =~ /darwin10/i
-      @platform = :macosx
-      require 'splat/darwin10_clipboard'
-      @clipboard =  Splat::Darwin10Clipboard.new
-      require 'splat/darwin10_launcher'
-      @launcher = Splat::Darwin10Launcher.new
+    case Config::CONFIG['host_os']
+      when /mswin|win32|dos|cygwin|mingw/i
+        @platform = :win32
+        require 'splat/win32_clipboard'
+        @clipboard = Splat::Win32Clipboard.new
+        require 'splat/win32_launcher'
+        @launcher = Splat::Win32Launcher.new
+        require 'watir'
+        @browser_class = Watir::IE
+      when /darwin10/i
+        @platform = :macosx
+        require 'splat/darwin10_clipboard'
+        @clipboard =  Splat::Darwin10Clipboard.new
+        require 'splat/darwin10_launcher'
+        @launcher = Splat::Darwin10Launcher.new
+        require 'safariwatir'
+        @browser_class = Watir::Safari
+      else
+        @platform = :unknown
+        puts "I have no idea how to cope with \"#{Config::CONFIG['host_os']}\""
     end
   end
 
@@ -25,6 +31,10 @@ class Splat
 
   def clipboard= content
     @clipboard.content = content if @clipboard
+  end
+
+  def browser
+    @browser_class.new if @browser_class
   end
 
   def launch content
