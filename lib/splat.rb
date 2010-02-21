0e5eb7caa0f3885be39c288bf1c8f1117a2fc922
Mark Ryall <mark@ryall.name>
2010-02-21 23:19:52 +1100

    added some exception handling around platform specific gems

--- lib/splat.rb
+++ lib/splat.rb
@@ -5,20 +5,36 @@ class Splat
     case Config::CONFIG['host_os']
       when /mswin|win32|dos|cygwin|mingw/i
         @platform = :win32
-        require 'splat/win32_clipboard'
-        @clipboard = Splat::Win32Clipboard.new
+        begin
+          require 'splat/win32_clipboard'
+          @clipboard = Splat::Win32Clipboard.new
+        rescue
+          puts 'for clipboard with splat on windows:'
+          puts ' * gem install win32-clipboard'
+        end
         require 'splat/win32_launcher'
         @launcher = Splat::Win32Launcher.new
-        require 'watir'
-        @browser_class = Watir::IE
+        begin
+          require 'watir'
+          @browser_class = Watir::IE
+        rescue
+          puts 'for browser automation with splat on windows'
+          puts ' * gem install watir'
+        end
       when /darwin10/i
         @platform = :macosx
         require 'splat/darwin10_clipboard'
         @clipboard =  Splat::Darwin10Clipboard.new
         require 'splat/darwin10_launcher'
         @launcher = Splat::Darwin10Launcher.new
-        require 'safariwatir'
-        @browser_class = Watir::Safari
+        begin
+          require 'safariwatir'
+          @browser_class = Watir::Safari
+        rescue
+          puts 'for browser automation with splat on mac os x:'
+          puts ' * gem install safariwatir'
+          puts ' * gem install rb-appscript'
+        end
       else
         @platform = :unknown
         puts "I have no idea how to cope with \"#{Config::CONFIG['host_os']}\""
